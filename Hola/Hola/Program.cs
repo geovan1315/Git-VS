27d188803967ff122b7d173cf4da3b08e5593730
Carlos Valladares <cvalladares@juanaparicio.edu.gt>
2020-03-24 15:57:56 -0600

    Primera ventana en consola

---
+++ Hola/Hola/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hola mundo");
+
+            Console.ReadKey();
+        }
+    }
+}
